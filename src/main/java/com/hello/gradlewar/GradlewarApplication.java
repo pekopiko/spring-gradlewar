02f98e39c19c8293ff5f4d13ee15640d408604c3
pekopiko <arimkim.global@gmail.com>
2024-05-29 17:50:57 +0900

    springboot gradle war project sample 1

---
+++ src/main/java/com/hello/gradlewar/GradlewarApplication.java
@@ -0,0 +1,13 @@
+package com.hello.gradlewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlewarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlewarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/gradlewar/GradlewarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.gradlewar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlewarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
